0602fd403fb380fdc47903ad5d926a4abb330e1b
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-08-15 14:44:13 +0000

    basic app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use leptos::*;
+
 fn main() {
-    println!("Hello, world!");
+    // mount_to_body(|| view! { <p>"Hello, world!"</p> })
+    leptos::mount_to_body(|| view! { <App/> })
+}
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+
+    view! {
+        <button
+            on:click=move |_| {
+                // on stable, this is set_count.set(3);
+                set_count.set(3);
+            }
+        >
+            "Click me: "
+            // on stable, this is move || count.get();
+            {move || count.get()}
+        </button>
+    }
 }
